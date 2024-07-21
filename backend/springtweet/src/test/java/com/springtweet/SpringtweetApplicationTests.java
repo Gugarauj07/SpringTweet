53caf72cae3c91d55d5ad54787f8b3d03a9f94b3
Gugarauj07 <gugaraujo01@gmail.com>
2024-07-21 09:25:01 -0400

    feat: connect to postgres

---
+++ backend/springtweet/src/test/java/com/springtweet/SpringtweetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtweet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtweetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
