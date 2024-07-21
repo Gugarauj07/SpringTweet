20c22ebaf8356b9fa749dbd6648430bfe0caebe5
Gugarauj07 <gugaraujo01@gmail.com>
2024-07-21 15:13:39 -0400

    feat: created role and user models

--- server/src/main/java/com/springtweet/server/ServerApplication.java
+++ server/src/main/java/com/springtweet/server/ServerApplication.java
@@ -2,9 +2,23 @@ package com.springtweet.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = {
+		SpringApplication.class,
+		Jsr310JpaConverters.class
+})
 public class ServerApplication {
+
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
