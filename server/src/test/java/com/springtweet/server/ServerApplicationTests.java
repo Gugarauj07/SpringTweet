47f20611fe74f6d819715e8fffc4939ce8f67ce6
Gugarauj07 <gugaraujo01@gmail.com>
2024-07-21 14:53:20 -0400

    feat: pooling app start

--- server/src/test/java/com/springtweet/server/ServerApplicationTests.java
+++ server/src/test/java/com/springtweet/server/ServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.springtweet;
+package com.springtweet.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringtweetApplicationTests {
+class ServerApplicationTests {
 
 	@Test
 	void contextLoads() {
