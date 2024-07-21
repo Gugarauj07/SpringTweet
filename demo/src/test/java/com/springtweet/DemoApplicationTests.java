efe707c4bd7c1033b10a42bfe55d1f21a2269338
gugarauj07 <gugaraujo01@gmail.com>
2024-07-20 23:13:20 -0400

    feat: start spring boot project

---
+++ demo/src/test/java/com/springtweet/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtweet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
