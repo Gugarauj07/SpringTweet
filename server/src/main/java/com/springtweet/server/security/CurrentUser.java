6f15fd6a3401871a7db640d407b18a50cb5950e4
Gugarauj07 <gugaraujo01@gmail.com>
2024-07-22 22:36:25 -0400

    feat: implementing the spring security

--- server/src/main/java/com/springtweet/server/security/CurrentUser.java
+++ server/src/main/java/com/springtweet/server/security/CurrentUser.java
@@ -1,2 +1,13 @@
-package com.springtweet.server.security;public class CurrentUser {
+package com.springtweet.server.security;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.PARAMETER, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@AuthenticationPrincipal
+public @interface CurrentUser {
+
 }
